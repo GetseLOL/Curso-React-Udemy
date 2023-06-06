cea720b9c7a81294a4ddda35810a931b12e983b5
Josue Getsemani Sandoval Villarreal <getsemanilol29@gmail.com>
2023-06-06 17:34:24 -0600

    Task 101579 - Actualizando repositorio

--- 08-Toolkit-Redux/src/store/store.js
+++ 08-Toolkit-Redux/src/store/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { counterSlice } from "./slices/counter/";
 import { pokemonSlice } from "./slices/pokemon";
 import { todosApi } from "./apis/todosApi";
+
 export const store = configureStore({
   reducer: {
     counter: counterSlice.reducer,
