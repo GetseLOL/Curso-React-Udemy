1f36b9f6a44061ccaab0a271ba9aebb9fa30e401
Josue Getsemani Sandoval Villarreal <getsemanilol29@gmail.com>
2023-06-06 17:33:43 -0600

    Task 101580 - Inicio de seccion Redux y Firebase

--- 07-JournalApp/src/auth/pages/RegisterPage.jsx
+++ 07-JournalApp/src/auth/pages/RegisterPage.jsx
@@ -1,12 +1,44 @@
 import { Link as RouterLink } from "react-router-dom";
-import { Google } from "@mui/icons-material";
 import { Button, Grid, Link, TextField, Typography } from "@mui/material";
 import { AuthLayout } from "../layout/AuthLayout";
+import { useForm } from "../../hooks";
+
+const formData = {
+  displayName: "Jhon Wick",
+  email: "jhonwick@gmail.com",
+  password: "daisy",
+};
+
+const formValidations = {
+  email: [(value) => value.includes("@"), "El correo debe tener una @"],
+  password: [
+    (value) => value.length >= 6,
+    "El password debe de ser mayor de 6 caracteres ",
+  ],
+  displayName: [(value) => value.length >= 1, "El nombre es obligatorio"],
+};
 
 export const RegisterPage = () => {
+  const {
+    formState,
+    displayName,
+    email,
+    password,
+    onInputChange,
+    onResetForm,
+    displayNameValid,
+    emailValid,
+    passwordValid,
+  } = useForm(formData, formValidations);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(formState);
+  };
+  console.log(displayNameValid);
   return (
     <AuthLayout title="Crear cuenta">
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
@@ -14,6 +46,11 @@ export const RegisterPage = () => {
               type="text"
               placeholder="John Wick"
               fullWidth
+              name="displayName"
+              value={displayName}
+              onChange={onInputChange}
+              error={!displayNameValid}
+              helperText={displayNameValid}
             />
           </Grid>
 
@@ -23,6 +60,9 @@ export const RegisterPage = () => {
               type="email"
               placeholder="correo@gmailcom"
               fullWidth
+              name="email"
+              value={email}
+              onChange={onInputChange}
             />
           </Grid>
 
@@ -32,11 +72,14 @@ export const RegisterPage = () => {
               type="password"
               placeholder="Contraseña"
               fullWidth
+              name="password"
+              value={password}
+              onChange={onInputChange}
             />
           </Grid>
           <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
             <Grid item xs={12}>
-              <Button variant="contained" fullWidth>
+              <Button type="submit" variant="contained" fullWidth>
                 Conectarse
               </Button>
             </Grid>
---
+++ 07-JournalApp/src/firebase/config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore/lite";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA5y2rNNJaRT_v-p56dxev_MaO2pLtJVC8",
+  authDomain: "journalapp-bf049.firebaseapp.com",
+  projectId: "journalapp-bf049",
+  storageBucket: "journalapp-bf049.appspot.com",
+  messagingSenderId: "725211876125",
+  appId: "1:725211876125:web:376ab9754189fb95b56ff4",
+};
+
+// Initialize Firebase  
+export const FireBaseApp = initializeApp(firebaseConfig);
+ 
+export const FireBaseAuth = getAuth(FireBaseApp);
+export const FireBaseDB = getFirestore(FireBaseApp);
