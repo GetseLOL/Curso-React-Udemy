36987a664c4bfa709b5dab33542b6bb4e49263c4
Josue Getsemani Sandoval Villarreal <getsemanilol29@gmail.com>
2023-06-08 11:04:10 -0600

    Task 101582 - Testing terminado, falta arreglar errores

--- 07-JournalApp/jest.setup.js
+++ 07-JournalApp/jest.setup.js
@@ -1,1 +1,13 @@
 import "whatwg-fetch"; // <-- yarn add whatwg-fetch
+import "setimmediate";
+
+// require("dotenv").config({
+//   path: ".env.test",
+// });
+
+// jest.mock("./src/helpers/getEnvironments", () => ({
+//   getEnvironments: () => ({ ...process.env }),
+// }));
+
+window.setImmediate = window.setTimeout;
+window.clearImmediate = window.clearTimeout;
--- 07-JournalApp/src/firebase/config.js
+++ 07-JournalApp/src/firebase/config.js
@@ -1,9 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore/lite";
+// import { getEnviroments } from "../helpers/getEnviroments";
 import { getFirestore } from "firebase/firestore/lite";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
+// const {
+//   VITE_APIKEY,
+//   VITE_AUTHDOMAIN,
+//   VITE_DATABASEURL,
+//   VITE_PROJECTID,
+//   VITE_STORAGEBUCKET,
+//   VITE_MESSAGINGSENDERID,
+//   VITE_APPID,
+// } = getEnviroments();
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,6 +26,15 @@ const firebaseConfig = {
   messagingSenderId: "725211876125",
   appId: "1:725211876125:web:376ab9754189fb95b56ff4",
 };
+
+// const firebaseConfig = {
+//   apiKey: VITE_APIKEY,
+//   authDomain: VITE_AUTHDOMAIN,
+//   projectId: VITE_PROJECTID,
+//   storageBucket: VITE_STORAGEBUCKET,
+//   messagingSenderId: VITE_MESSAGINGSENDERID,
+//   appId: VITE_APPID,
+// };
 
 // Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig);
